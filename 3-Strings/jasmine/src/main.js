b3e7ae5aa90d0989a02fed0721001387ffdc1853
CyberCook07 <walkercook2007@gmail.com>
2025-09-17 13:32:14 -0500

    3-String Assignment

---
+++ 3-Strings/jasmine/src/main.js
@@ -0,0 +1,14 @@
+// Task 1
+let line = "Stand so high Huge hooves too Impatiently waits for Reins and harness Eager to leave";
+const frontDoorLetter = line[0]; 
+
+// Task 2
+const frontDoorPassword = frontDoorLetter + line[14].toLowerCase() + line[30].toLowerCase() + line[52].toLowerCase() + line[80].toLowerCase()
+let frontDoorWord = frontDoorPassword
+
+// Task 3
+let lineWithWhiteSpace = 'Reins and harness'
+let backDoorLetter = lineWithWhiteSpace[16]
+
+// Task 4
+let backDoorPassword = `Hor${backDoorLetter}e` + `, please`
